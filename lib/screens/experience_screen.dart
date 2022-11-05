85e04d25c01dadbf036dd4a3a12e95137dcacc37
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-05 09:40:52 +0100

    finished the work experience card

--- lib/screens/experience_screen.dart
+++ lib/screens/experience_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_cv/utils/colors.dart';
+import 'package:mobile_cv/utils/constants.dart';
 
 class ExperienceScreen extends StatelessWidget {
   const ExperienceScreen({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class ExperienceScreen extends StatelessWidget {
         centerTitle: true,
         title: Text(
           'Experience',
-          style: GoogleFonts.lato(fontSize: 20, fontWeight: FontWeight.w500),
+          style: GoogleFonts.lato(fontSize: 22, fontWeight: FontWeight.w600),
         ),
         toolbarHeight: 100,
         backgroundColor: AppColors.prussianBlue,
@@ -33,7 +34,139 @@ class ExperienceScreen extends StatelessWidget {
             bottom: 20,
           ),
           child: Column(
-            children: [],
+            children: [
+              Card(
+                elevation: 5,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+                child: Container(
+                  margin: const EdgeInsets.all(10),
+                  height: 200,
+                  width: double.infinity,
+                  child: ListView(
+                    //crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Work Experience',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 17),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: List.generate(
+                            150 ~/ 2,
+                            (index) => Expanded(
+                                  child: Container(
+                                    color: index % 2 == 0
+                                        ? Colors.transparent
+                                        : Colors.grey,
+                                    height: 1,
+                                  ),
+                                )),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'TT',
+                            style: kExperiencetagTextStyle,
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Flutter Mobile Developer',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 16),
+                              ),
+                              const Text(
+                                'TechTink Solutions Ltd, Abuja',
+                                style: TextStyle(
+                                    fontSize: 14, fontWeight: FontWeight.w500),
+                              ),
+                              Text(
+                                'July 2022 - Date | 4 months',
+                                style: TextStyle(color: Colors.grey.shade700),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            'PW',
+                            style: kExperiencetagTextStyle,
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Chief Product Officer',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 16),
+                              ),
+                              const Text(
+                                'ParkWell Africa, Lagos',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 14),
+                              ),
+                              Text(
+                                'Nov 2021 - April 2022| 5 months',
+                                style: TextStyle(color: Colors.grey.shade700),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        children: [
+                          Text('KP', style: kExperiencetagTextStyle),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Flutter Mobile Developer',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 16),
+                              ),
+                              const Text(
+                                'Kwikpay Credit, Lagos',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 14),
+                              ),
+                              Text(
+                                'Aug 2022 - Nov 2021',
+                                style: TextStyle(color: Colors.grey.shade700),
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+
+final kExperiencetagTextStyle = GoogleFonts.aclonica(
+  fontSize: 20,
+  color: const Color.fromARGB(255, 195, 80, 35),
+);
