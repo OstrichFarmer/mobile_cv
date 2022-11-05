aa676c33cd3256acb8d9ba5e45cc36baaba0b1e0
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-05 18:14:52 +0100

    finished the profile screen custom appbar

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mobile_cv/utils/colors.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.lightGrey,
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(240),
+          child: Stack(
+            children: [
+              Container(
+                height: 165,
+                color: AppColors.prussianBlue,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: const Icon(
+                        Icons.light_mode,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    )
+                  ],
+                ),
+              ),
+              const Align(
+                alignment: Alignment.bottomCenter,
+                child: CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.white,
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/profile.jpg'),
+                    radius: 90,
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile_cv/utils/colors.dart';
 
 final kExperiencetagTextStyle = GoogleFonts.aclonica(
   fontSize: 20,
-  color: const Color.fromARGB(255, 195, 80, 35),
+  color: AppColors.prussianBlue,
 );
