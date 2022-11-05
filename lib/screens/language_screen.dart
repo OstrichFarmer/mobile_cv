af5fec356820f0b10c45355ff69ee86d1d2b8401
OstrichFarmer <joel.stephen56.sj@gmail.com>
2022-11-05 22:07:40 +0100

    created a language button

---
+++ lib/screens/language_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../utils/colors.dart';
+
+class LanguageScreen extends StatelessWidget {
+  const LanguageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar:  AppBar(
+        centerTitle: true,
+        title: Text(
+          'Languages',
+          style: GoogleFonts.lato(fontSize: 25, fontWeight: FontWeight.w600),
+        ),
+        toolbarHeight: 70,
+        backgroundColor: AppColors.prussianBlue,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(15),
+              bottomRight: Radius.circular(15)),
+        ),
+      ),,);
+  }
+}
